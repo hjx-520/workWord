1135fecf2e1253a63972e3e216d7c1beb1778da0
hjx-520 <80037768+hjx-520@users.noreply.github.com>
2026-01-12 09:40:59 +0800

    Add files via upload

---
+++ ARRDataImport/ARRDataImport/ARRDataImport.vb
@@ -0,0 +1,97 @@
+﻿Imports log4net
+Imports System.Data.OleDb
+Imports System.IO
+Imports BEA.TFS.Common.Entities
+Imports BEA.TFS.Common.ExtensionMethods
+Imports BEA.TFS.CommonUtility
+
+Public Module ARRDataImport
+
+    Private ReadOnly _logger As ILog = LogManager.GetLogger(GetType(ARRDataImport))
+    ''' <summary>
+    ''' This list mimics TI440 behaviour where all functions will try-catch every exception, log the error message and continue processing with best effort. 
+    ''' This list will be send in an email at the end of this program.
+    ''' </summary>
+    Friend _errorMessageList As New List(Of String)
+    Public Function Main(Optional args As String() = Nothing) As Integer
+        Try
+            _logger.Info($"Start {NameOf(ARRDataImport)}...")
+            _logger.Info("Initializing...")
+
+            Dim dsINIFile As String = Path.GetFullPath(My.Settings.DATA_SOURCE_INI_FILE)
+            Dim iniFile As String = Path.GetFullPath(My.Settings.INI_FILE)
+            Dim emailINIFile As String = Path.GetFullPath(My.Settings.EMAIL_INI_FILE)
+            _logger.Info("Reading datasource config...")
+            Dim dbConfig As DataSourceConfig = GetDataSourceConfig(dsINIFile)
+            _logger.Info("Reading email config...")
+            Dim emailConfig As EmailConfig = GetEmailConfig(emailINIFile)
+            _logger.Info("Reading program config...")
+            Dim config As INIConfig = GetConfig(iniFile)
+            Dim rptConnStr As String = GetConnectionStr(dbConfig.RptDSN, dbConfig.RptUserID, dbConfig.RptPwd)
+
+            Using conn As New OleDbConnection(rptConnStr)
+                conn.Open()
+                ' Do not start transaction here, start transaction for each module and commit on success/rollback on error
+                Try
+                    ' Get working date
+                    Dim rptRunDate As RptEntities.RptRunDate = RptEntities.RptRunDate.Find(conn, Nothing)
+                    TryLogPropertyValues(rptRunDate)
+
+                    Dim workingDate As Date = rptRunDate.RunDate.Value
+                    Dim nextWorkingDate As Date = rptRunDate.NextBusinessDate.Value
+
+                    ' Note: Using ResolveFileName() will remove all '<' or '>'. Perform manual replace first.
+                    ' For detailed test, refer To case 2 of unit test "Test_ResolveFileName_Resolving_Two_Different_Dates()"
+                    config.FILE_ARR = config.FILE_ARR.Replace("<yyMMdd_end>", workingDate.ToString("yyMMdd"))
+                    config.FILE_ARR = ResolveFileName(config.FILE_ARR, workingDate)
+
+
+                    _logger.Debug($"Completed updating date element in files, logging file names...")
+                    config.TryLogPropertyValues()
+
+
+                    ' ARR
+                    ImportARR(config, conn, workingDate)
+
+                    _logger.Info($"End {NameOf(ARRDataImport)}...")
+
+
+                    'Delibrately not roll back due to each module has different behaviour.
+                Finally
+
+                    ' This is equivalent to the logic "If (error_flag) Then"
+                    ' If any error occurred, send email with error messages in email body.
+                    If _errorMessageList.Count > 0 Then
+                        _logger.Info($"Sending error notification email...")
+                        SendErrorEmail(emailConfig, config, _errorMessageList)
+                        _logger.Info($"Error notification sent.")
+                    End If
+                End Try
+            End Using
+
+
+        Catch ex As Exception
+            _logger.Error($"Error occured in {NameOf(ARRDataImport)} : ", ex)
+            Return -1
+        End Try
+
+        Return 0
+
+    End Function
+
+    Public Function GetConfig(iniFile As String) As INIConfig
+        Dim config As New INIConfig With {
+            .EmailTo = GetProfileStringVal("ARRDataImport", "EmailTo", iniFile),
+            .EmailCCTo = GetProfileStringVal("ARRDataImport", "EmailCCTo", iniFile),
+            .EmailSubject = GetProfileStringVal("ARRDataImport", "EmailSubject", iniFile),
+            .EmailToErr = GetProfileStringVal("ARRDataImport", "EmailToErr", iniFile),
+            .EmailCCToErr = GetProfileStringVal("ARRDataImport", "EmailCCToErr", iniFile),
+            .EmailSubjectErr = GetProfileStringVal("ARRDataImport", "EmailSubjectErr", iniFile),
+            .FILE_ARR = GetProfileStringVal("ARRDataImport", "FILE_ARR", iniFile),
+            .SFTPINFOLDER = GetProfileStringVal("ARRDataImport", "SFTPINFOLDER", iniFile),
+            .SFTPOUTFOLDER = GetProfileStringVal("ARRDataImport", "SFTPOUTFOLDER", iniFile)
+        }
+
+        Return config
+    End Function
+End Module
---
+++ ARRDataImport/ARRDataImport/FileEntity/ARRFile.vb
@@ -0,0 +1,89 @@
+﻿''' <summary>
+''' Defines the structure for file ARRFile
+''' </summary>
+Public Class ARRFile
+    Public Property RATE_CODE As Decimal
+    Public Property FCC As Decimal
+    Public Property CONVENT_TYPE As String
+    Public Property K_DAYS As String
+    Public Property END_DATE As String
+    Public Property START_DATE As String
+    Public Property DAYS_OF_INT As String
+    Public Property COMP_RATE_UNADJ As String
+    Public Property SIM_RATE_UNADJ As Decimal
+    Public Property COMP_RATE_ADJ As Decimal
+    Public Property SIM_RATE_ADJ As Decimal
+    Public Property FLOOR_RATE As Decimal
+    Public Property FLOOR_IND As String
+    Public Property COMP_RATE_ADJ_CAS_ON As Decimal
+    Public Property SIM_RATE_ADJ_CAS_ON As Decimal
+    Public Property FLOOR_IND_CAS_ON As Decimal
+    Public Property COMP_RATE_ADJ_CAS_1W As Decimal
+    Public Property SIM_RATE_ADJ_CAS_1W As Decimal
+    Public Property FLOOR_IND_CAS_1W As Decimal
+    Public Property COMP_RATE_ADJ_CAS_1M As Decimal
+    Public Property SIM_RATE_ADJ_CAS_1M As Decimal
+    Public Property FLOOR_IND_CAS_1M As String
+    Public Property COMP_RATE_ADJ_CAS_2M As Decimal
+    Public Property SIM_RATE_ADJ_CAS_2M As Decimal
+    Public Property FLOOR_IND_CAS_2M As String
+    Public Property COMP_RATE_ADJ_CAS_3M As Decimal
+    Public Property SIM_RATE_ADJ_CAS_3M As Decimal
+    Public Property FLOOR_IND_CAS_3M As String
+    Public Property COMP_RATE_ADJ_CAS_6M As Decimal
+    Public Property SIM_RATE_ADJ_CAS_6M As Decimal
+    Public Property FLOOR_IND_CAS_6M As String
+    Public Property COMP_RATE_ADJ_CAS_12M As Decimal
+    Public Property SIM_RATE_ADJ_CAS_12M As Decimal
+    Public Property FLOOR_IND_CAS_12M As String
+    Public Property RUN_DATE As Date
+
+
+
+    ''' <summary>
+    ''' Decode a line read from ARRFile into different fields.
+    ''' </summary>
+    ''' <param name="line">One single body line read from file ARRFile</param>
+    ''' <returns></returns>
+    Public Shared Function ReadLine(line As String) As ARRFile
+        Dim ARRFileObject As New ARRFile()
+
+        ' Run_Date and LastCalendarDate is not read from the line
+        ARRFileObject.RATE_CODE = line.Substring(6, 8).Trim
+        ARRFileObject.FCC = line.Substring(14, 3).Trim
+        ARRFileObject.CONVENT_TYPE = line.Substring(17, 5).Trim
+        ARRFileObject.K_DAYS = line.Substring(22, 3).Trim
+        ARRFileObject.END_DATE = Convert.ToDateTime(line.Substring(25, 10).Trim)
+        ARRFileObject.START_DATE = Convert.ToDateTime(line.Substring(35, 10).Trim)
+        ARRFileObject.DAYS_OF_INT = If(line.Substring(45, 5).Trim = "", 0, Convert.ToInt16(line.Substring(45, 5).Trim))
+        ARRFileObject.COMP_RATE_UNADJ = If(line.Substring(50, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(50, 11).Trim))
+        ARRFileObject.SIM_RATE_UNADJ = If(line.Substring(61, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(61, 11).Trim))
+        ARRFileObject.COMP_RATE_ADJ = If(line.Substring(72, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(72, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ = If(line.Substring(83, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(83, 11).Trim))
+        ARRFileObject.FLOOR_RATE = If(line.Substring(94, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(94, 11).Trim))
+        ARRFileObject.FLOOR_IND = line.Substring(105, 1).Trim
+        ARRFileObject.COMP_RATE_ADJ_CAS_ON = If(line.Substring(106, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(106, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ_CAS_ON = If(line.Substring(117, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(117, 11).Trim))
+        ARRFileObject.FLOOR_IND_CAS_ON = line.Substring(128, 1).Trim
+        ARRFileObject.COMP_RATE_ADJ_CAS_1W = If(line.Substring(129, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(129, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ_CAS_1W = If(line.Substring(140, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(140, 11).Trim))
+        ARRFileObject.FLOOR_IND_CAS_1W = line.Substring(151, 1).Trim
+        ARRFileObject.COMP_RATE_ADJ_CAS_1M = If(line.Substring(152, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(152, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ_CAS_1M = If(line.Substring(163, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(163, 11).Trim))
+        ARRFileObject.FLOOR_IND_CAS_1M = line.Substring(174, 1).Trim
+        ARRFileObject.COMP_RATE_ADJ_CAS_2M = If(line.Substring(175, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(175, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ_CAS_2M = If(line.Substring(186, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(186, 11).Trim))
+        ARRFileObject.FLOOR_IND_CAS_2M = line.Substring(197, 1).Trim
+        ARRFileObject.COMP_RATE_ADJ_CAS_3M = If(line.Substring(198, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(198, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ_CAS_3M = If(line.Substring(209, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(209, 11).Trim))
+        ARRFileObject.FLOOR_IND_CAS_3M = line.Substring(220, 1).Trim
+        ARRFileObject.COMP_RATE_ADJ_CAS_6M = If(line.Substring(221, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(221, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ_CAS_6M = If(line.Substring(232, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(232, 11).Trim))
+        ARRFileObject.FLOOR_IND_CAS_6M = line.Substring(243, 1).Trim
+        ARRFileObject.COMP_RATE_ADJ_CAS_12M = If(line.Substring(244, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(244, 11).Trim))
+        ARRFileObject.SIM_RATE_ADJ_CAS_12M = If(line.Substring(255, 11).Trim = "", 0D, Convert.ToDecimal(line.Substring(255, 11).Trim))
+        ARRFileObject.FLOOR_IND_CAS_12M = line.Substring(266, 1).Trim
+
+        Return ARRFileObject
+    End Function
+End Class
